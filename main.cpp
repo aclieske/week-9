37a9da802bfb0f6505ae7c53d56e9353f042238f
aclieske <112497756+aclieske@users.noreply.github.com>
2022-10-28 02:11:06 +0000

    final code

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@ void printmatrix(double arr[60][7]);
 
 int main() {
 
-  double arr[60][7]={{1,100,100,100,100},{2,100,0,100,0},{82,94,73,86},{64,74,84,94},{94,84,74,64}};
+  double arr[60][7]={{1,100,100,100,100},{2,100,0,100,0},{3,82,94,73,86},{4,64,74,84,94},{5,94,84,74,64}};
 
-  
+  for(int j=0;j<5;j++)
+    arr[j][5]=((arr[j][1]*0.2)+(arr[j][2]*0.3)+(arr[j][3]*0.3)+(arr[j][4]*0.2));
 
-
-  
+  printmatrix(arr);
 }
 void printmatrix(double arr[60][7]){
   
-  cout<<setw(15)<<"Student"<<setw(15)<<"Grade 1"<<setw(15)<<"Grade 2"<<setw(15)<<"Grade 3"<<setw(15)<<"Grade 4"<<setw(15)<<"Final Grade"
-  for(int i=0; i<60; i++)
+  cout<<setw(12)<<"Student"<<setw(12)<<"Grade 1"<<setw(12)<<"Grade 2"<<setw(12)<<"Grade 3"<<setw(12)<<"Grade 4"<<setw(12)<<"Grade Final"<<endl;
+  for(int i=0; i<5; i++)
     {
-      for(int j=0;j<7;j++)
+      for(int j=0;j<6;j++)
         {
-          cout<<setw(10)<<arr[i][j];
+          cout<<setw(12)<<arr[i][j];
         }
       cout<<endl;
     }
