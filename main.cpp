4e58dce3eee17298a70845e5fcadac1b15c378e8
aclieske <112497756+aclieske@users.noreply.github.com>
2022-10-28 02:04:34 +0000

    initialized array

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@ using namespace std;
 void printmatrix(double arr[60][7]);
 
 int main() {
+
+  double arr[60][7]={{1,100,100,100,100},{2,100,0,100,0},{82,94,73,86},{64,74,84,94},{94,84,74,64}};
+
+  
+
+
   
 }
 void printmatrix(double arr[60][7]){
   
-  cout<<setw(10)<<"Student"<<setw(10)<<"Grade 1"<<setw(10)<<"Grade 2"<<setw(10)<<"Grade 3"<<setw(10)<<"Grade 4";
+  cout<<setw(15)<<"Student"<<setw(15)<<"Grade 1"<<setw(15)<<"Grade 2"<<setw(15)<<"Grade 3"<<setw(15)<<"Grade 4"<<setw(15)<<"Final Grade"
   for(int i=0; i<60; i++)
     {
       for(int j=0;j<7;j++)
